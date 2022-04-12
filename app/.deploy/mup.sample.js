7c6fc837466f6f18964a4a3d1370ad450d467c99
Kevin Nahinu <kevinan@hawaii.edu>
2022-04-11 22:16:45 -1000

    update mup

--- app/.deploy/mup.sample.js
+++ app/.deploy/mup.sample.js
@@ -1,26 +1,41 @@
 module.exports = {
   servers: {
     one: {
-      host: '111.222.333.444',
+      host: '137.184.229.210',
       username: 'root',
       password: 'changeme'
     }
   },
+
   app: {
-    // if you edit the app 'name' field, be sure to run 'mup stop' if the app is already running.
-    // otherwise you will have two apps deployed at once, with unpredictable results.
     name: 'meteor-application-template-react',
     path: '../',
-    servers: { one: {}, },
-    buildOptions: { serverOnly: true },
+
+    servers: {
+      one: {},
+    },
+
+    buildOptions: {
+      serverOnly: true,
+    },
+
     env: {
-      ROOT_URL: 'http://111.222.333.444',
+      ROOT_URL: 'http://137.184.229.210',
       MONGO_URL: 'mongodb://mongodb/meteor',
       MONGO_OPLOG_URL: 'mongodb://mongodb/local',
     },
-    docker: { image: 'zodern/meteor:latest' },
+
+    docker: {
+      image: 'zodern/meteor:latest',
+    },
+
     enableUploadProgressBar: true
   },
-  mongo: { version: '5.0.5', servers: { one: {} }
+
+  mongo: {
+    version: '3.4.1',
+    servers: {
+      one: {}
+    }
   },
 };
